082fa588cfb0b07b8ad0a75167cab8b947074c1e
hp <namstudionsk@gmail.com>
2018-02-08 23:22:34 +0700

    Fix lv

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -6,27 +6,40 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-/*import org.slf4j.Logger;
+//import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.util.logging.PlatformLogger;*/
+import sun.util.logging.PlatformLogger;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+//import java.util.logging.*;
+import java.util.logging.LogManager;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class Main {
 
-    //public static Logger log = LoggerFactory.getLogger(Main.class);
+    public static org.slf4j.Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 	// write your code here
 
-      /*  log.info("Info");
+        log.info("Info");
 
         log.error("Error");
 
         log.warn("Warn");
 
-        log.debug("Debug");*/
+        log.debug("Debug");
+
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
+        Logger llg = Logger.getLogger("global");
+        llg.setLevel(Level.FINEST);
+
+
+        llg.fine("FIne");
+
 
         try{
             Main mn = new Main();
@@ -47,29 +60,43 @@ public class Main {
         }
         catch (SlickException ex)
         {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public class GameApp extends BasicGame{
+
+        private Logger llg;
+
         public GameApp(String gamename){
             super(gamename);
+
+            llg = Logger.getLogger("global");
+            llg.setLevel(Level.FINEST);
         }
 
         @Override
         public void init(GameContainer gc) throws SlickException{
             //
+            llg.fine("INIT");
+
+            log.info("init");
+
         }
 
         @Override
         public void update(GameContainer gc, int i) throws SlickException{
             //
+            llg.fine("UPDATE");
+
+            log.info("update");
+
         }
 
         @Override
         public void render(GameContainer gc, Graphics g) throws SlickException{
             g.drawString("Hello Fuck you", 100, 100);
-            g.drawString("Пидор сука", 200, 200);
+
         }
     }
 
