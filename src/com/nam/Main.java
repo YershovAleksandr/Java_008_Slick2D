389179e5f7123b668ca08c29493eaed27bcd15b1
hp <namstudionsk@gmail.com>
2018-02-08 22:19:22 +0700

    Add all libs

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -1,8 +1,24 @@
 package com.nam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    public static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 	// write your code here
+
+        log.info("Info");
+
+        log.error("Error");
+
+        log.warn("Warn");
+
+        log.debug("Debug");
+
+
+
     }
 }
